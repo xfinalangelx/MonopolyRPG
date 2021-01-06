db0c9d0f8e72f526d85317dd625558a31e9d68ac
finalangel <55591218+xfinalangelx@users.noreply.github.com>
2021-01-06 10:25:08 +0800

    Added Music to the game

--- src/MonopolyRPG/Main.java
+++ src/MonopolyRPG/Main.java
@@ -1,13 +1,22 @@
 package MonopolyRPG;
 
 import MonopolyRPG.Controller.GameModePage;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application {
+    private static MusicList musicList;
     public static void main(String[] args) {
-
+        musicList = new MusicList();
+        musicList.play();
         GameModePage gameModePage=  new GameModePage();
         gameModePage.start();
 
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
